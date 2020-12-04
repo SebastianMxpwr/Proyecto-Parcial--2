4e8a8313a6f2bb4f8ef7e4fa522572109b8daf50
Sebastian <190807@utags.edu.mx>
2020-12-03 23:04:06 -0600

    Ultimos detalles

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.get('/', function(req, res){
-    res.send('<h1>Bienvenido a mi servidor rest (localhost)</h1>');
+    res.send('<h1>Bienvenido a mi servidor rest</h1>');
 })
 
 app.use(require('./routes/usuario'));
